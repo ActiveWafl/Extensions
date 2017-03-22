f48c95b3a12df8d0308fef7990ea6ef1210a66c2
Evan de la Cruz <evan@wafl.org>
2017-03-22 11:59:35 -0700

    Option to include seconds in localtime_format

--- Templating/Smarty3/Plugins/modifier.localtime_format.php
+++ Templating/Smarty3/Plugins/modifier.localtime_format.php
@@ -1,13 +1,23 @@
-<?php 
-function smarty_modifier_localtime_format($timestamp)
+<?php
+function smarty_modifier_localtime_format($timestamp, $includeSeconds = false)
 {
     $app = \Wafl\CORE::$RUNNING_APPLICATION;
     $tzOffsetSecs = $app->Get_ClientGmtOffset();
-    $localTimeString = strftime("%l:%M&nbsp;%p", $timestamp+$tzOffsetSecs);
-	 if (trim($localTimeString) == "") //windows doesnt support %e or %l
-	 {
-		$localTimeString = strftime("%I:%M&nbsp;%p", $timestamp+$tzOffsetSecs);
-	 }
-	 return $localTimeString;
+    if ($includeSeconds)
+    {
+        $localTimeString = strftime("%l:%M:%S&nbsp;%p", $timestamp+$tzOffsetSecs);
+    } else {
+        $localTimeString = strftime("%l:%M&nbsp;%p", $timestamp+$tzOffsetSecs);
+    }
+    if (trim($localTimeString) == "") //windows doesnt support %e or %l
+    {
+        if ($includeSeconds)
+        {
+            $localTimeString = strftime("%I:%M:%S&nbsp;%p", $timestamp+$tzOffsetSecs);
+        } else {
+            $localTimeString = strftime("%I:%M&nbsp;%p", $timestamp+$tzOffsetSecs);
+        }
+    }
+    return $localTimeString;
 }
 ?>
